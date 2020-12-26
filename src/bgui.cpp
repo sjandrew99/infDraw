d7963c57f478de8ad29cc4457097cb8e9b41020e
Stephen Andrew <stephenandrew99@gmail.com>
2020-12-26 10:28:48 -0500

    added menuBar# Please enter the commit message for your changes. Lines starting

--- src/bgui.cpp
+++ src/bgui.cpp
@@ -23,21 +23,35 @@ BGui::~BGui()
 int BGui::setup()
 {    
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_container_set_border_width(GTK_CONTAINER(window),4);
-    
+    //gtk_container_set_border_width(GTK_CONTAINER(window),4);
+    gtk_window_set_title(GTK_WINDOW(window),"infDraw");
     g_signal_connect(window,"key-release-event",G_CALLBACK(Callbacks::key_event),this);
     
-    winGrid = gtk_grid_new(); gtk_widget_show(winGrid); gtk_container_add(GTK_CONTAINER(window),winGrid);
     
+    winGrid = gtk_grid_new(); gtk_container_add(GTK_CONTAINER(window),winGrid);
+    
+    //MENU (http://zetcode.com/gui/gtk2/menusandtoolbars/):
+    GtkWidget * menuBar = gtk_menu_bar_new(); //gtk_widget_show(menuBar);
+    gtk_grid_attach(GTK_GRID(winGrid),menuBar,0,0,1,1);
+    GtkWidget * fileMenu = gtk_menu_new();
+    GtkWidget * fileMi = gtk_menu_item_new_with_label("File");
+    GtkWidget * quitMi = gtk_menu_item_new_with_label("Quit");
+    g_signal_connect(G_OBJECT(quitMi), "activate",
+        G_CALLBACK(on_window_closed), this);
+    gtk_menu_item_set_submenu(GTK_MENU_ITEM(fileMi),fileMenu);
+    gtk_menu_shell_append(GTK_MENU_SHELL(fileMenu),quitMi);
+    gtk_menu_shell_append(GTK_MENU_SHELL(menuBar),fileMi);   
+       
     notebook = new TabbedNotebook(this,winGrid,0,1);
     notebook->addTab();
     
-    gtk_widget_show(window);
     g_signal_connect(window,"destroy",G_CALLBACK(on_window_closed),this);
-    this->statusMsg = gtk_label_new("SELECT"); gtk_widget_show(this->statusMsg);
+    this->statusMsg = gtk_label_new("SELECT"); //gtk_widget_show(this->statusMsg);
     gtk_grid_attach(GTK_GRID(winGrid),statusMsg,0,2,1,1);
     closed = 0;
 
+
+    gtk_widget_show_all(window);
     return 0;
 
 
