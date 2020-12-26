328d59516e41d2ddadf6df8a778731935def2a50
Stephen Andrew <stephenandrew99@gmail.com>
2020-12-26 10:28:29 -0500

    split tabbedNotebook class into its own header file

--- include/bgui.h
+++ include/bgui.h
@@ -7,44 +7,12 @@
 #include "controlPanel.h"
 #include "imageViewPort.h"
 #include "imageManager.h"
-
+#include "tabbedNotebook.h"
 struct cv_point_t
 {
  int x; int y;
 };
 
-class TabbedNotebook; //forward declare 
-class Tab
-{
-  public:
-  Tab(TabbedNotebook * parent);
-  GtkWidget * grid;
-  ControlPanel * controlPanelTop;
-  ControlPanel * controlPanelRight;
-  ControlPanel * controlPanelBottom;
-  ControlPanel * controlPanelLeft;
-  ControlPanel * controlPanelLookup(int idx);
-  ImageViewPort * imageViewPort; 
-  int set_image(cv::Mat * image);
-  TabbedNotebook * parent;
-  ImageManager * imgMgr;
-};
-
-class BGui;
-class TabbedNotebook
-{
- public:
-  TabbedNotebook(BGui * parent, GtkWidget *,int,int);
-  int addTab();
-  Tab * getActiveTab();
-  BGui * parent;
- private:
-  std::vector<Tab *> tabs;
-  GtkWidget * notebook;
-  int nTabs;
-  int activeTab;
-  
-};
 
 class Application;
 class BGui
---
+++ include/tabbedNotebook.h
@@ -0,0 +1,46 @@
+#ifndef TABBED_NOTEBOOK_H_
+#define TABBED_NOTEBOOK_H_
+#include <opencv2/opencv.hpp>
+#include <gtk/gtk.h>
+#include <vector>
+
+#include "controlPanel.h"
+#include "imageViewPort.h"
+#include "imageManager.h"
+
+
+class TabbedNotebook; //forward declare 
+class Tab
+{
+  public:
+  Tab(TabbedNotebook * parent);
+  GtkWidget * grid;
+  ControlPanel * controlPanelTop;
+  ControlPanel * controlPanelRight;
+  ControlPanel * controlPanelBottom;
+  ControlPanel * controlPanelLeft;
+  ControlPanel * controlPanelLookup(int idx);
+  ImageViewPort * imageViewPort; 
+  int set_image(cv::Mat * image);
+  TabbedNotebook * parent;
+  ImageManager * imgMgr;
+};
+
+class BGui;
+class TabbedNotebook
+{
+ public:
+  TabbedNotebook(BGui * parent, GtkWidget *,int,int);
+  int addTab();
+  Tab * getActiveTab();
+  BGui * parent;
+ private:
+  std::vector<Tab *> tabs;
+  GtkWidget * notebook;
+  int nTabs;
+  int activeTab;
+  
+};
+
+
+#endif //ifndef BGUI_H
